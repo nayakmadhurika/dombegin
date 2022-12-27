efb4bd62fd97d8e92fb833dfdaf7aa1bc26ecaf6
nayakmadhurika <nayakmadhurika6@gmail.com>
2022-12-27 13:25:17 +0530

    added bold font

--- script.js
+++ script.js
@@ -32,8 +32,9 @@ var items = document.getElementsByClassName('list-group-item');
 items[1].textContent = 'hello 2';
 items[1].style.fontWeight = 'bold';
 items[1].style.backgroundColor = 'yellow';
+items[2].style.backgroundColor = 'green';
 
 
 for(var i = 0; i < items.length; i++){
-    items[i].style.backgroundColor = '#f4f4f4';
+    items[i].style.fontWeight = 'bold';
 }
