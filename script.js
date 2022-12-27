eafbd9f20ce9e8893d4d772620bed0e43cf3d636
nayakmadhurika <nayakmadhurika6@gmail.com>
2022-12-27 17:47:29 +0530

    query selector

--- script.js
+++ script.js
@@ -51,8 +51,19 @@ var header = document.getElementById('main-header');
 //     li[i].style.fontWeight = 'bold';
 // }
 
+//QUERYSELECTOR
 var secondItem = document.querySelector('.list-group-item:nth-child(2)');
 secondItem.style.backgroundColor = 'green';
 
 var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
 thirdItem.style.display = 'none';
+
+//QUERYSELECTORALL
+var secItem = document.querySelectorAll('.list-group-item');
+secItem[1].style.color = 'green'; 
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = 'green'; 
+}
