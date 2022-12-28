06c69a11d050687ff756fb82bb139998a961ee1b
nayakmadhurika <nayakmadhurika6@gmail.com>
2022-12-28 18:46:31 +0530

    added discription box through dom

--- script.js
+++ script.js
@@ -1,155 +1,7 @@
-// // //Examine the document object
-
-// // // console.log(document.domain);
-// // // console.log(document.URL);
-// // // console.log(document.title);
-// // // console.log(document.doctype);
-// // // console.log(document.head);
-// // // console.log(document.body);
-// // // console.log(document.all);
-// // //console.log(document.all[10]);
-// // //document.all[10].textContent = 'Hello';
-// // //console.log(document.images);
-
-// // //Get element by id
-// // //console.log(document.getElementById('header-title'));
-// // var headerTitle = document.getElementById('header-title');
-// // var header = document.getElementById('main-header');
-// // //console.log(headerTitle);
-// // // headerTitle.textContent = 'Hello';
-// // // headerTitle.innerText = 'goodbye';
-// //  //headerTitle.innerHTML = '<h3>hello</h3>'
-// //  headerTitle.style.borderBottom = 'solid 3px black';
-// //  header.style.borderBottom = 'solid 3px black';
-
-// //  var addItem = document.getElementById('addItems');
-// //  addItem.style.fontWeight = 'bold';
-// //  addItem.style.color = 'green';
-
-
-// // //get element by class name
-// // // var items = document.getElementsByClassName('list-group-item');
-// // // items[1].textContent = 'hello 2';
-// // // items[1].style.fontWeight = 'bold';
-// // // items[1].style.backgroundColor = 'yellow';
-// // // items[2].style.backgroundColor = 'green';
-
-
-// // // for(var i = 0; i < items.length; i++){
-// // //     items[i].style.fontWeight = 'bold';
-// // // }
-
-// // // //get element by tagname //
-// // // var li = document.getElementsByTagName('li');
-// // // li[1].textContent = 'hello 2';
-// // // li[1].style.fontWeight = 'bold';
-// // // li[1].style.backgroundColor = 'yellow';
-// // // li[2].style.backgroundColor = 'green';
-
-
-// // // for(var i = 0; i < li.length; i++){
-// // //     li[i].style.fontWeight = 'bold';
-// // // }
-
-// // //QUERYSELECTOR
-// // var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-// // secondItem.style.backgroundColor = 'green';
-
-// // var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-// // thirdItem.style.display = 'none';
-
-// // //QUERYSELECTORALL
-// // var secItem = document.querySelectorAll('.list-group-item');
-// // secItem[1].style.color = 'green'; 
-
-// // var odd = document.querySelectorAll('li:nth-child(odd)');
-
-// // for(var i = 0; i < odd.length; i++){
-// //     odd[i].style.backgroundColor = 'green'; 
-// // }
-
-// //TRAVERSING THE DOM
-// var itemList = document.querySelector('#items');
-
-// //parentElement
-// console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentElement.parentElement);
-
-// //childNodes
-// console.log(itemList.children);
-// console.log(itemList.children[1]);
-// itemList.children[1].style.backgroundColor = 'yellow';
-
-// //FirstChild
-// console.log(itemList.firstChild);
-// //FirstElementChild
-// console.log(itemList.firstElementChild);
-// itemList.firstElementChild.textContent = 'hello1';
-
-// //lastChild
-// console.log(itemList.lastChild);
-// //lastElementChild
-// console.log(itemList.lastElementChild);
-// itemList.lastElementChild.textContent = 'hello4'
-
-// //NEXTSIBLING
-// console.log(itemList.nextSibling);
-// //NEXTELEMENT SIBLING
-// console.log(itemList.nextElementSibling);
-
-// //PREVIOUS SIBLING
-// console.log(itemList.previousSibling);
-// //previous element sibling
-// console.log(itemList.previousElementSibling);
-// itemList.previousElementSibling.style.color = 'green';
-
-// //CREATE ELEMENT
-// //create a div
-// var newDiv = document.createElement('div');
-
-// //add class
-// newDiv.className = 'hello';
-
-// // add ID
-//  newDiv.id = 'hello1';
-
-// //set attribute
-//  newDiv.setAttribute('title', 'hello Div');
-
-// //create a text node
-// var newDivText = document.createTextNode('hello world');
-
-// //add text to div
-// newDiv.appendChild(newDivText);
-
-// var container = document.querySelector('header .container');
-// var h1 = document.querySelector('header h1');
-
-
-// console.log(newDiv);
-
-// container.insertBefore(newDiv, h1);
-
-// var newLi = document.createElement('li');
-// newLi.className = 'list-group-item';
-
-
-// var newLiText = document.createTextNode('hello world');
-
-// newLi.appendChild(newLiText);
-
-// var cont = document.querySelector('div .list-group')
-// var ul = document.querySelector('div li');
-
-// console.log(newLi);
-
-// cont.insertBefore(newLi, ul);
-
-
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var li = document.getElementsByTagName('li');
+var filter = document.getElementById('filter');
 
 //create edit button for existing li
 for(var i = 0; i < li.length; i++){
@@ -163,6 +15,8 @@ for(var i = 0; i < li.length; i++){
 form.addEventListener('submit', addItem);
 //Delete event
 itemList.addEventListener('click', removeItem);
+//Filter event
+filter.addEventListener('keyup', filterItems);
 
 //add item
 function addItem(e){
@@ -170,12 +24,16 @@ function addItem(e){
 
     //get input value
     var newItem = document.getElementById('item').value;
+    var description = document.getElementById('description').value;
+
     //create new li element
     var li = document.createElement('li');
     //add class name
     li.className = 'list-group-item';
     //add text node with input value
-    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newItem + " " + description));
+    // li.appendChild(document.createTextNode(" " + description));
+    console.log(newItem,description);
 
     //create delete button element
     var deleteBtn = document.createElement('button');
@@ -205,3 +63,23 @@ function removeItem(e){
         }
     }
 }
+
+//filter items
+function filterItems(e){
+    //convert to lowercase
+    var text = e.target.value.toLowerCase();
+    //get lis
+    var items = itemList.getElementsByTagName('li');
+    console.log(items);
+    // convert to an array
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        console.log(item);
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
+
