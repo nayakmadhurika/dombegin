f527c6b71e3768830e4a604309fa6cac910edbfc
nayakmadhurika <nayakmadhurika6@gmail.com>
2022-12-29 17:19:28 +0530

    added user using json

--- form.js
+++ form.js
@@ -1,25 +1,25 @@
-function storage(event){
-    event.preventDefault();
-    const name=event.target.username.value;
-    const email=event.target.emailId.value;
-    const number=event.target.phoneno.value;
-    localStorage.setItem('name',name);
-    localStorage.setItem('emailId',email);
-    localStorage.setItem('PhoneNo',number);
-    }
-
-
 // function storage(event){
-// //Get the input values
-// let username = document.getElementById('username').value;
-// let email = document.getElementById('email').value;
+//     event.preventDefault();
+//     const name=event.target.username.value;
+//     const email=event.target.emailId.value;
+//     const number=event.target.phoneno.value;
+//     localStorage.setItem('name',name);
+//     localStorage.setItem('emailId',email);
+//     localStorage.setItem('PhoneNo',number);
+//     }
 
-// //Create a JSON object
-// let userDetails = {
-//     "username" : username,
-//     "email" : email
-// };
 
-// //Store the user details in localstorage
-// localStorage.setItem('user', JSON.stringify(userDetails));
-// }
+function storage(event){
+//Get the input values
+let username = document.getElementById('username').value;
+let email = document.getElementById('email').value;
+
+//Create a JSON object
+let userDetails = {
+    "username" : username,
+    "email" : email
+};
+
+//Store the user details in localstorage
+localStorage.setItem('user', JSON.stringify(userDetails));
+}
